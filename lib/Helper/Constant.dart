a8af11fda5933e232b54726d482099414fd2ff4c
AshishCodexxa <ashish@codexxa.in>
2023-08-31 11:49:13 +0530

    add all project

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,25 @@
+const String appName = 'Kitchen Cartage';
+
+const String packageName = 'com.kitchencartage.customer';
+const String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+const String iosPackage = 'com.wrteam.eshop';
+const String iosLink = 'your ios link here';
+const String appStoreId = '123456789';
+
+const String deepLinkUrlPrefix = 'https://eshopwrteamin.page.link';
+const String deepLinkName = 'eshop.com';
+
+const int timeOut = 50;
+const int perPage = 10;
+
+//Token ExpireTime in minutes & issuer name
+const int tokenExpireTime = 5;
+const String issuerName = 'eshop';
+
+const String baseUrl = 'https://kitchencartage.com/app-admin/app/v1/api/';
+const String jwtKey = "65c9cd19cd138f19ddf2f6320c7a802ee936c548";
+
+const String defaultCountryCode = 'IN';
+
+const int decimalPoints = 2;
